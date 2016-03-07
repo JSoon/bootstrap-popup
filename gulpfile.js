1c1f2c591d1ed98cd3f139de199a1c40f2764889
jsoon <serdeemail@gmail.com>
2016-03-07 16:46:23 +0800

    build bootstrap-popup.js into dist

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@
 var gulp = require('gulp');
 var sourcemaps = require('gulp-sourcemaps');
 var uglify = require('gulp-uglify');
+var strip = require('gulp-strip-comments');
 var rename = require('gulp-rename');
 var header = require('gulp-header');
 var package = require('./package.json');
@@ -20,6 +21,11 @@ gulp.task('JS', function () {
     var files = ['bootstrap-popup.js'];
 
     return gulp.src(files)
+        .pipe(strip())
+        .pipe(header(banner, {
+            pkg: package
+        }))
+        .pipe(gulp.dest('dist'))
         .pipe(rename('bootstrap-popup.min.js'))
         .pipe(gulp.dest('dist'))
         .pipe(sourcemaps.init())
